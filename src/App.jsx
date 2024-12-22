fd6a672832da93ff5907ce9807a03da9c35cfdad
abhishekn1208 <abhisheknigam735@gmail.com>
2024-12-23 01:39:24 +0530

    sign

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import Sigin from "./Component/Auth/SIgnin/sigin";
+
 import SIgnUp from "./Component/Auth/Signup/SIgnUp";
 import Home from "./Component/Home/Home";
 import Navbar from "./Component/Navbar/Navbar";
 import PokemonCard from "./Component/PokemonCard/PokemonCard";
 import {Routes,Route} from 'react-router-dom'
 import PrivateRoute from "./Component/PrivateRoute/PrivateRoute";
+import Sigin from "./Component/Auth/sign_in/sigin";
+
 
 function App() {
 
@@ -17,7 +19,7 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/pokemon" element={<PrivateRoute><PokemonCard/></PrivateRoute>}/>
       <Route path="/signup" element={<SIgnUp />}/>
-      <Route path="/sigin" element={<Sigin />}/>
+      <Route path="/sigin" element={<Sigin/>}/>
     </Routes>
       
     </>
